5005a887019c62ff77ecd917552018dbb3d64db4
Joe Martella <martellaj@live.com>
2016-01-28 16:08:17 -0800

    :video_game: Moves computation to controller.

--- client/main/main.controller.js
+++ client/main/main.controller.js
@@ -15,6 +15,10 @@
     vm.input2 = null;
 
     // Methods
+    vm.add = add;
+    vm.subtract = subtract;
+    vm.multiply = multiply;
+    vm.divide = divide;
 
     /**
      * This function does any initialization work the
@@ -23,5 +27,25 @@
     (function activate () {
       $log.debug('Activated MainController.');
     })();
+    
+    function add (a, b) {
+      return a + b;
+    }
+    
+    function subtract (a, b) {
+      return a - b;
+    }
+    
+    function multiply (a, b) {
+      return a * b;
+    }
+    
+    function divide (a, b) {
+      if (b === 0) {
+        return 'can\'t do that';
+      } else {
+        return a / b;
+      }
+    }
   }
 })();
